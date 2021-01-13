184e0db0136d234e53521cb5e9fd6d2cb28452fd
Artyom Terentiev <terentiev.art@yandex.ru>
2021-01-13 16:24:27 +0300

    change background and buttons styles

--- client/src/components/Mode/Mode.js
+++ client/src/components/Mode/Mode.js
@@ -11,18 +11,17 @@ export default function Mode(props) {
                 <h2 className="header col s12 light">Choose voting mode</h2>
             </div>
             <div className="row center">
-                <a
-                    className="waves-effect waves-light btn-large"
+                <a  
+                    className="btn-large cst-btn"
                     name="simple"
                     onClick={handleClick}
                 >Simple mode</a>
             </div>
             <div className="row center">
                 <a
-                    className="waves-effect waves-light btn-large"
+                    className="btn-large cst-btn-disabled"
                     name="pro"
-                    onClick={handleClick}
-                    disabled
+                    onClick={(e) => {e.preventDefault()}}
                 ><i className="material-icons left">lightbulb_outline</i>Pro mode (under construction)
                 </a>
             </div>
